7a338894390b07724da079f89e2fc1555af4d01e
Adrenaline990 <medtahatmimi@gmail.com>
2025-02-10 12:59:14 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    h2 {
+        text-align: center;
+}
+
+</style>
+<body>
+<fieldset style="width: 300px;margin-left: auto;margin-right: auto;">
+    <h2>Sign in</h2>
+    <input type="text" id="Username" placeholder="Username" required><br><br>
+    <input type="text" id="password" placeholder="Password" required><br><br>
+    <h6> Don't have an account? Sign in</h6>
+    <button type="submit">Valider</button>
+</br>
+</fieldset>
+</body>
+</html>
